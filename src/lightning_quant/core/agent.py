ef56a7832b8c24d633817d0eb18d438a486912f9
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2023-07-11 20:07:44 -0400

    refactor

--- src/lightning_quant/core/agent.py
+++ src/lightning_quant/core/agent.py
@@ -18,7 +18,7 @@ from typing import List, Union
 from zoneinfo import ZoneInfo
 
 from lightning_quant.pipeline.acquisition_engineer import AcquisitionEngineer
-from lightning_quant.pipeline.brute import BruteForceOptimizer
+from lightning_quant.pipeline.brute_optimizer import BruteForceOptimizer
 from lightning_quant.pipeline.feature_engineer import FeatureEngineer
 from lightning_quant.pipeline.label_engineer import LabelEngineer
 
